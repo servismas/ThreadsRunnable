c23ff656e8973b64f3a6c94d30d3a3862ccc4611
servismas <servismas@gmail.com>
2020-01-29 19:52:06 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		Thread t3 = new Thread(r3);
 		Thread t4 = new Thread(r4);
 		Thread t5 = new Thread(r5);
+		
 		t1.setDaemon(true);
 		t2.setDaemon(true);
 		t3.setDaemon(true);
@@ -24,14 +25,16 @@ public class Main {
 		t3.start();
 		t4.start();
 		t5.start();
+		
 		try {
-			t1.join(3000);
-			t2.join(3000);
-			t3.join(3000);
-			t4.join(3000);
-			t5.join(3000);
+			t1.join(1000);
+			t2.join(1000);
+			t3.join(1000);
+			t4.join(1000);
+			t5.join(1000);
 		} catch (Exception e) {
 		}
+		
 		System.out.println(r1.getCounter());
 		System.out.println(r2.getCounter());
 		System.out.println(r3.getCounter());
